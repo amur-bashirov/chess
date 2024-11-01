f990a130080dcf5fb79ee1ee946b1958359ca0b7
Amur Bashirov <bashirov@byu.edu>
2024-11-01 09:27:30 -0600

    wrote most of tests files

--- server/src/test/java/dataaccess/DataBaseManagerTests.java
+++ server/src/test/java/dataaccess/DataBaseManagerTests.java
@@ -1,4 +1,5 @@
 package dataaccess;
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.*;
 
@@ -6,6 +7,7 @@ import org.junit.jupiter.api.*;
 public class DataBaseManagerTests {
     private static DatabaseManager db;
     private final UserDataAccess userAccess = new MySqlUserDAO();
+    private final AuthDataAccess authAccess = new MySqlAuthDAO();
 
     public DataBaseManagerTests() throws DataAccessException {
     }
@@ -71,6 +73,82 @@ public class DataBaseManagerTests {
         UserData data2 = userAccess.getUser(data.username());
         Assertions.assertEquals(data, data2);
         userAccess.clear();
+    }
+
+    @Test
+    public void successGetAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        AuthData auth2 = authAccess.getAuth(auth.authToken());
+        Assertions.assertEquals(auth, auth2);
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void failGetAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        authAccess.deleteAuth(auth);
+        AuthData auth2 = authAccess.getAuth(auth.authToken());
+        Assertions.assertNull(auth2);
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void failCreateAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        Assertions.assertThrows(DataAccessException.class, () -> authAccess.createAuth(data));
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void successCreateAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        AuthData auth2 = authAccess.getAuth(auth.authToken());
+        Assertions.assertEquals(auth, auth2);
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void successDeleteAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        authAccess.deleteAuth(auth);
+        AuthData auth2 = authAccess.getAuth(auth.authToken());
+        Assertions.assertNull(auth2);
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void failDeleteAuthTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        AuthData auth = authAccess.createAuth(data);
+        authAccess.deleteAuth(auth);
+        Assertions.assertThrows(DataAccessException.class, () -> authAccess.deleteAuth(auth));
+        userAccess.clear();
+        authAccess.clear();
+    }
+
+    @Test
+    public void successClearTest() throws DataAccessException {
+        UserData data = new UserData("test", "test", "test");
+        userAccess.creatUser(data);
+        userAccess.clear();
+        UserData data2 = userAccess.getUser(data.username());
+        Assertions.assertNull(data2);
     }
 
 }
